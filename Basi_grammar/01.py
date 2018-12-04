baa1f3fe8d001b6a013ebcacf0a099fceb6366be
Jason <yuanjie081215@qq.com>
2018-12-04 10:22:34 +0800

    Update 01.py

--- Basi_grammar/01.py
+++ Basi_grammar/01.py
@@ -5,3 +5,21 @@ if number_1 >= number_2:
     print(number_1)
 else:
     print(number_2)
+
+
+    
+
+# 给定一个不超过5位的正整数,判断其有几位
+number = int(input("Please enter a number not exceeding five digits: "))
+if number >= 1000:
+    if number >= 10000:
+        print("What you type is: five digits")
+    else:
+        print("What you type is: four digits")
+else:
+    if number >= 100:
+        print("What you type is: three digits")
+    elif number >= 10:
+        print("What you type is: two digits")
+    else:
+        print("What you type is: one digits")
