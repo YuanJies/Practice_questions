071f29aa83911543ddb8b4be083259f6fbffdd33
Jason <yuanjie081215@qq.com>
2018-12-04 10:14:48 +0800

    Update 01.py

--- Basi_grammar/01.py
+++ Basi_grammar/01.py
@@ -1,1 +1,7 @@
 # 输入2个数字,输出最大数
+number_1 = int(input("Please enter the first number： "))
+number_2 = int(input("Please enter the second number: "))
+if number_1 >= number_2:
+    print(number_1)
+else:
+    print(number_2)
