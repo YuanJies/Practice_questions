87d876d0f486862806d00a2995fcbc6c9a95c711
Jason <yuanjie081215@qq.com>
2018-12-04 11:22:53 +0800

    Update 03.py

--- Basic_grammar/03.py
+++ Basic_grammar/03.py
@@ -6,6 +6,7 @@ for i in range(number-2):
 print("*" * number)
 
 #---------------------------------------------------------------------
+
 number = int(input("Please enter the side length of the square: "))
 e = -number  // 2
 for i in range(e,number+e):
@@ -15,6 +16,7 @@ for i in range(e,number+e):
         print("*" + " " * (number - 2) + "*")
        
 #-----------------------------------------------------------------------
+
 等价于上一个
 number = int(input("Please enter the side length of the square: "))
 for i in range(number):
