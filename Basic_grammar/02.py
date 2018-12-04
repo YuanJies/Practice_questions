866353205c5330eb4c171fe22bb2189d8311e2d3
Jason <yuanjie081215@qq.com>
2018-12-04 10:41:13 +0800

    Create 02.py

---
+++ Basic_grammar/02.py
@@ -0,0 +1,19 @@
+# 给定一个不超过5位的正整数,判断该数的位数,依次打印出个位、十位、百位、千位、万位的数字
+number = int(input("Please enter a positive integer no more than five digits: "))
+if number >= 1000:
+    if number >= 10000:
+        num = 5
+    else:
+        num = 4
+else:
+    if number >= 100:
+        num = 3
+    elif number >= 10:
+        num = 2
+    else:
+        num = 1
+a = number
+for i in range(num):
+    b = a % 10
+    a //= 10
+    print(b)
