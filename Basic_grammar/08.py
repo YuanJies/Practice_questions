9277bda71e7ca698a51fe08c3378995608d1792d
Jason <yuanjie081215@qq.com>
2018-12-04 15:40:33 +0800

    打印对顶三角形

--- Basic_grammar/08.py
+++ Basic_grammar/08.py
@@ -6,3 +6,16 @@
 #  ***
 # *****
 #*******
+n = 7
+e = n//2
+for i in range(-e,n-e):
+    prespace = -i if i<0 else i
+    print(" "*(e-prespace)+ "*"*(2*prespace+1))
+    
+#------------------------------------------------------
+
+a = int(input("Please enter a number: "))
+for i in range(-a,a+1):
+    if i < 0:
+        i = -i
+    print(" "*(a-i)+"*"*(2*i+1)+" "*(a-i))
